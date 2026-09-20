be5eae8a79c80891ef1802f37a2a272ebb8f9192
Mahfoozdev <mahfoozdoit@gmail.com>
2026-09-20 20:16:57 +0500

    new favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -74,9 +74,9 @@ export const metadata: Metadata = {
   },
   manifest: "/manifest.json",
   icons: {
-    icon: [{ url: "/favicon.svg", type: "image/svg+xml" }],
-    shortcut: "/favicon.svg",
-    apple: [{ url: "/favicon.svg", type: "image/svg+xml" }],
+    icon: [{ url: "/favicon.png", type: "image/svg+xml" }],
+    shortcut: "/favicon.png",
+    apple: [{ url: "/favicon.png", type: "image/svg+xml" }],
   },
   robots: {
     index: true,
